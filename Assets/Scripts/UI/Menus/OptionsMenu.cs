325395f08fe6f74b58d3520b97ecabc958d69ffc
RodrigoOrea <rodrigosiete@outlook.es>
2025-06-01 20:26:29 +0200

    Some UI improved: Pause window + help, discovered an issue when there are multiple placemarks with the same name

--- Assets/Scripts/UI/Menus/OptionsMenu.cs
+++ Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -5,12 +5,26 @@ public class OptionsMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void ToggleFullscreen()
+{
+    if (Screen.fullScreen)
+    {
+        Screen.fullScreenMode = FullScreenMode.Windowed;
+        Screen.fullScreen = false;
+    }
+    else
+    {
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        Screen.fullScreen = true;
+    }
+}
+
 }
